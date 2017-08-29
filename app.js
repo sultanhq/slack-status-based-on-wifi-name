3aaa78cd46ff126480b616534087801ec580e4bb
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-29 16:47:30 +0100

    now updates Slack channel with branch name

--- app.js
+++ app.js
@@ -1,93 +1,62 @@
 "use strict";
 
 require('dotenv').config()
-const os = require('os');
 const axios = require("axios");
-const execSync = require("child_process").execSync;
 const querystring = require("querystring");
 const config = require("./config");
+const path = require('path');
+
+var status = {};
+var oldStatus = {};
+
+var getGitBranchName = require('git-branch-name');
+
+var dirPath = path.resolve('', config.path);
+
+
 
 if (!config.slackToken) {
-    console.error("Missing Slack token. Set it in config.js");
-    process.exit(1);
+  console.error("Missing Slack token. Set it in config.js");
+  process.exit(1);
 }
 
-function getLinuxWiFiName() {
-    return execSync("iwgetid -r") // Linux only
-            .toString()
-            .split("\n")
-            .filter(line => line.match(/.+/))
-            .find(ssid => true); // find first
+
+function getBranchName() {
+  getGitBranchName(dirPath, function(err, branch) {
+    myBranch(branch)
+  });
+  return status
 }
 
-function getMacWiFiName() {
-    return execSync("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I") // macos only
-            .toString()
-            .split("\n")
-            .filter(line => line.includes(" SSID: "))
-            .map(line => line.match(/: (.*)/)[1])
-            .find(ssid => true); // find first
-}
-
-function getWinWiFiName() {
-    return execSync("netsh wlan show interfaces") // Windows only
-            .toString()
-            .split("\n")
-            .filter(line => line.includes(" SSID "))
-            .map(line => line.match(/: (.*)/)[1])
-            .find(ssid => true); // find first
+function myBranch(branch) {
+  oldStatus = status
+  status = {
+    "status_text": branch,
+    "status_emoji": ":office:"
+  };
 }
 
 function setSlackStatus(token, status) {
-    return axios.post("https://slack.com/api/users.profile.set",
-        querystring.stringify({
-            token: token,
-            profile: JSON.stringify(status)
-        }), {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          }
-        }).then(function(response) {
-            console.log("Set Slack status API response: %j", response.data);
-        })
-        .catch(function(error) {
-            console.error("Set Slack status error: %s", error);
-        });
+  return axios.post("https://slack.com/api/users.profile.set",
+      querystring.stringify({
+        token: token,
+        profile: JSON.stringify(status)
+      }), {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }).then(function(response) {
+      console.log("Set Slack status branch name to: %j", response.data.profile.status_text);
+    })
+    .catch(function(error) {
+      console.error("Set Slack status error: %s", error);
+    });
 }
 
-const platform = os.platform();
-
-let wiFiName;
-let getWiFiName;
-// Get appropriate function for platform
-switch (platform) {
-  case 'darwin':
-    getWiFiName = getMacWiFiName;
-    break;
-  case 'win32':
-    getWiFiName = getWinWiFiName;
-    break;
-  case 'linux':
-    getWiFiName = getLinuxWiFiName;
-    break;
-  default:
-    console.error('Unknown platform %s', platform);
-    process.exit(2);
-}
 
 setInterval(function() {
-    const newWiFiName = getWiFiName();
-    if (newWiFiName === wiFiName) {
-        return;
-    }
-    wiFiName = newWiFiName;
-    console.log("Connected WiFi SSID: %s", wiFiName);
-
-    const status = config.statusByWiFiName[wiFiName];
-    if (!status) {
-        console.log("Status not specified for WiFi: %s", wiFiName);
-        return;
-    }
-    console.log("Setting Slack status to: %j", status);
+  const newBranchName = getBranchName();
+  if (status.status_text !== oldStatus.status_text) {
     setSlackStatus(config.slackToken, status);
+  }
 }, config.updateInterval);
--- config.js
+++ config.js
@@ -1,14 +1,5 @@
 module.exports = {
-    slackToken: process.env["SLACK_TOKEN"], // xoxp-...
-    statusByWiFiName: {
-        "Home": {
-            "status_text": "Working remotely",
-            "status_emoji": ":house:"
-        },
-        "Office": {
-            "status_text": "In the office",
-            "status_emoji": ":office:"
-        }
-    },
-    updateInterval: 1000 // every second
+    slackToken: process.env["SLACK_TOKEN"],
+    path: '../../Work/Code/mobillitwo', 
+    updateInterval: 3000 // every 3 seconds
 }
